3716e94051318e4fce99f21e94464fb08fe3dec6
akashm199 <akashm199@gmail.com>
2018-12-21 11:03:34 +0530

    Akash, this is my first commit.

---
+++ src/myrepotest/MyRepoTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myrepotest;
+
+/**
+ *
+ * @author akash
+ */
+public class MyRepoTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        print();
+    }
+
+    private static void print() {
+        System.out.println("Test 1");
+    }
+}
